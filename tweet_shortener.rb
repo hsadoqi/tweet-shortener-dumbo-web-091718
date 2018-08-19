0fa7894a0e6523529459857a05df6d264a8e0038
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-19 23:21:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,18 @@
 
 def dictionary {
   "hello" => "hi", 
-  "to" => 2, 
-  "two" => 2, 
-  
+  "to" => "2", 
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
 }
+
+def word_substituter(string)
+  word_substituter.each do |x|
+
+
