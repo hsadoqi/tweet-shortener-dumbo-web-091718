4722d6ce0441d5d3e4d62e92bc01ba22249c8145
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-20 00:20:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -43,7 +43,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if tweet.length > 140
+  if word_substituter(tweet).length > 140
     word_substituter[0..136] + "..."
   else
     tweet
